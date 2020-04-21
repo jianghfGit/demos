19f72b3e1901f311796730c807453c2faa4e006f
Xiaojiang <zzjianghaifeng@163.com>
2020-04-21 22:00:42 +0800

    Xiaojiang

---
+++ src/main/java/cn/itcast/web/HelloController.java
@@ -0,0 +1,24 @@
+package cn.itcast.web;
+
+import cn.itcast.pojo.User;
+import cn.itcast.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("user")
+public class HelloController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("{id}")
+    public User findById(@PathVariable("id") Long id){
+        return userService.queryById(id);
+    }
+}
